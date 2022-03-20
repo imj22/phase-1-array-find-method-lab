0a40f53957af865cdb5b7a2777116addc4a5f343
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-20 15:45:45 +0100

    commit tree 7aabbe7d381c2d0e0ea931256f18729ede9fa5ee

--- index.js
+++ index.js
@@ -1,1 +1,94 @@
 // code your solution here
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"},
+    { year: "2004", result: "N/A"},
+    { year: "2003", result: "N/A"},
+    { year: "2002", result: "N/A"},
+    { year: "2001", result: "N/A"},
+    { year: "2000", result: "N/A"},
+    { year: "1999", result: "N/A"},
+    { year: "1998", result: "W"},
+    { year: "1997", result: "W"},
+    { year: "1996", result: "N/A"},
+    { year: "1995", result: "N/A"},
+    { year: "1994", result: "N/A"},
+    { year: "1993", result: "N/A"},
+    { year: "1992", result: "N/A"},
+    { year: "1991", result: "N/A"},
+    { year: "1990", result: "N/A"},
+    { year: "1989", result: "L"},
+    { year: "1988", result: "N/A"},
+    { year: "1987", result: "L"},
+    { year: "1986", result: "L"},
+    { year: "1985", result: "N/A"},
+    { year: "1984", result: "N/A"},
+    { year: "1983", result: "N/A"},
+    { year: "1982", result: "N/A"},
+    { year: "1981", result: "N/A"},
+    { year: "1980", result: "N/A"},
+    { year: "1979", result: "N/A"},
+    { year: "1978", result: "N/A"},
+    { year: "1977", result: "N/A"},
+    { year: "1976", result: "L"},
+    { year: "1975", result: "N/A"},
+    { year: "1974", result: "N/A"},
+    { year: "1973", result: "N/A"},
+    { year: "1972", result: "N/A"},
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "N/A"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "N/A"},
+    { year: "1965", result: "N/A"},
+    { year: "1964", result: "N/A"},
+    { year: "1963", result: "N/A"},
+    { year: "1962", result: "N/A"},
+    { year: "1961", result: "N/A"},
+    { year: "1960", result: "N/A"}
+  ]
+
+  // superbowlWin should:
+// - recieve one argument (an array of objects, aka record data)
+        // NOTE: each object has TWO properties, "year" and "result"
+// - use .find() to test each object to see if the result is a win (aka "W")
+// - return the year when the win occurred
+// - if no win is found, it should return undefined.
+
+function superbowlWin(recordObj) {
+    const winResult = recordObj.find(recordObj => recordObj.result === "W") // winResult finds the FULL object associated with all the wins via .find
+    return winResult ? winResult.year : undefined; // 
+    //     return recordObj.year;
+    // } else {(recordObj.result === "L", "N/A")
+    //     return undefined;
+    // }
+}
+// const result = inventory.find( ({ name }) => name === 'cherries' )
+// const winRecord = record.map(superbowlWin)
+// console.log(winRecord)
+
+console.log(superbowlWin(record))
+// console.log(record.find(superbowlWin))
+
+
+// winFinder(record, year){
+//     if(record === "W"){
+//         return year;
+//     } else if (record === "L", "N/A") {
+//         return null;
+//     }
+// }
